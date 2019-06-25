79c807e979ed4c22d644496433a70f6cd2574a9f
Victor Araujo <varaujo@fb.com>
2019-06-25 15:04:15 -0700

    working basic project

---
+++ app/src/main/java/com/codepath/MainActivity.java
@@ -0,0 +1,85 @@
+package com.codepath;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        // initializes items
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                Log.i("MainActivity", "Removed item " + position);
+                return true;
+            }
+        });
+    }
+
+    // returns the file in which the data is stored
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    // read the items from the file system
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) (findViewById(R.id.etNewItem));
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+}
